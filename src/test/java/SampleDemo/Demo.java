2a8b102855871f0d6a4ab6f61d5da73aed0d35c6
sathiyab <sathiyabalaji07@gmail.com>
2020-06-22 00:29:31 +0530

    commit

--- src/test/java/SampleDemo/Demo.java
+++ src/test/java/SampleDemo/Demo.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Demo {
@@ -14,7 +15,7 @@ public class Demo {
 	//static driver
 	
 		@Test(priority=1)
-		public void Demo() throws IOException {
+		public void Demo() throws IOException, InterruptedException {
 		
 		// TODO Auto-generated method stub
 		
@@ -30,19 +31,37 @@ public class Demo {
 		driver.findElement(By.xpath("//*[@ng-model='Phone']")).sendKeys("9790840572");
 		driver.findElement(By.xpath("//*[@value='Male']")).click();
 		driver.findElement(By.xpath("//*[@value='Cricket']")).click();
-		Select Languages=new Select(driver.findElement(By.id("msdd")));
-		Languages.selectByIndex(5);
-
+		//Select Languages=new Select(driver.findElement(By.id("msdd")));
+		//Select Languages=new Select(driver.findElement(By.xpath("//*[@id='msdd']")));
+		//Languages.selectByIndex(5);
+		driver.findElement(By.xpath("//*[@id='msdd']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//a[text()='Arabic']")).click();
 		Select Skills=new Select(driver.findElement(By.id("Skills")));
 		Skills.selectByIndex(5);
+		
+		try {
 		Select countries=new Select(driver.findElement(By.id("countries")));
 		countries.selectByIndex(5);
+		driver.findElement(By.xpath("//span[@class='select2-selection select2-selection--single']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//li[text()='Australia1']")).click();
+		}catch(Exception e)
+		{System.out.println("Exception handled" +e);
+		}
+
+
+		
 	//	Select country=driver.findElement(By.xpath("//*[@class='select2-selection select2-selection--single']"));
 		//country.selectByIndex(5);
 		driver.findElement(By.xpath("//*[@id='yearbox']")).sendKeys("1980");
 		driver.findElement(By.xpath("//*[@placeholder='Month']")).sendKeys("06");
 		driver.findElement(By.xpath("//*[@id='daybox']")).sendKeys("07");
+		
 		driver.findElement(By.id("firstpassword")).sendKeys("Balaji");
+		boolean pwd=driver.findElement(By.id("firstpassword")).isDisplayed();
+		Assert.assertTrue(pwd);
+		
 		driver.findElement(By.id("secondpassword")).sendKeys("Balaji");
 		driver.findElement(By.id("submitbtn")).click();
 		
